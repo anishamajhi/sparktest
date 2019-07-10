53b40e16d9de8dca1f64c846b4f499d874bde8c0
Anisha Onion <anisha@onionlife.in>
2019-07-10 17:07:40 +0530

    first commit

---
+++ __run__.py
@@ -0,0 +1,10 @@
+class test:
+<<<<<<< HEAD
+	print("Hello")
+	while(1==1):
+   		print("Hello loop")
+=======
+        print("Hello")
+        while(1==1):
+                print("Hello loop")
+>>>>>>> ce8be4424df58004697df3ad686a93ba032bfb94
---
+++ process2.py
@@ -0,0 +1,4 @@
+class test:
+	print("Hello")
+	while(1==1):
+   		print("Hello loop")
